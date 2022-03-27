4fd1ea1e4a4c769e1d80f92fe9907bd30f945ec3
Felice Moretta <felice_moretta@hotmail.it>
2022-03-27 22:50:10 +0200

    Corretto bug annotazione, eliminata stringa PCSCOPRO%23 presente, per alcuni individui, nei fatti prolog. Aggiunta sostituzione nello script solve_path.

--- Prolog/Utility/solve_path.py
+++ Prolog/Utility/solve_path.py
@@ -6,7 +6,7 @@ with open('/Users/fefox/Desktop/PROGETTO - Supply Chain/Prolog/fatti.pl', 'r') a
 text = text.replace('http://localhost:8060/BPMNSemanticAnnotator/resources/files/BPMN2_Ontology.owl#', 'http://193.206.100.151/annotatorFiles/AnnotatoreSemanticoClient/CartelleUtenti/BPMN2_Ontology.owl#')
 text = text.replace('http://localhost:8080/BPMNSemanticAnnotator/resources/files/MM_Ontology.owl#','http://193.206.100.151/annotatorFiles/AnnotatoreSemanticoClient/CartelleUtenti/MM_Ontology.owl#')
 text = text.replace('http://localhost:8080/BPMNSemanticAnnotator/resources/files/MM_Ontology.owl','http://193.206.100.151/annotatorFiles/AnnotatoreSemanticoClient/CartelleUtenti/MM_Ontology.owl')
-
+text = text.replace('PCSCOPRO%23','')
 with open('/Users/fefox/Desktop/PROGETTO - Supply Chain/Prolog/fatti.pl', 'w') as f:
     f.write(text)
     print("DONE.")
