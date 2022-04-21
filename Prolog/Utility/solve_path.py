a835156008f2ea6f46ec97674731ea1e2690e83d
Felice Moretta <felice_moretta@hotmail.it>
2022-04-21 12:17:36 +0200

    Modificate indentazioni dei due file contenenti le regole di supporto per la relazione.

--- Prolog/Utility/solve_path.py
+++ Prolog/Utility/solve_path.py
@@ -1,4 +1,4 @@
-# replace text in file with new text
+# NOTE: replace text in file with new text
 with open('/Users/fefox/Desktop/PROGETTO - Supply Chain/Prolog/fatti.pl', 'r') as f:
     text = f.read()
     #print(text)
@@ -7,6 +7,7 @@ text = text.replace('http://localhost:8060/BPMNSemanticAnnotator/resources/files
 text = text.replace('http://localhost:8080/BPMNSemanticAnnotator/resources/files/MM_Ontology.owl#','http://193.206.100.151/annotatorFiles/AnnotatoreSemanticoClient/CartelleUtenti/MM_Ontology.owl#')
 text = text.replace('http://localhost:8080/BPMNSemanticAnnotator/resources/files/MM_Ontology.owl','http://193.206.100.151/annotatorFiles/AnnotatoreSemanticoClient/CartelleUtenti/MM_Ontology.owl')
 text = text.replace('PCSCOPRO%23','')
+
 with open('/Users/fefox/Desktop/PROGETTO - Supply Chain/Prolog/fatti.pl', 'w') as f:
     f.write(text)
     print("DONE.")
